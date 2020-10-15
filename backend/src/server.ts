485cdfc9693455b9d4a5f0dcf49dbca0f8a657cd
xsadia <lipearthur@hotmail.com>
2020-10-15 17:21:11 -0300

    api finalizada :monkey:

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,12 +3,16 @@ import 'express-async-errors';
 import routes from './routes';
 import './database/connection';
 import path from 'path';
+import errorHandler from './errors/handler';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
 app.listen(3333, () => {
     console.log("servindo no port 3333");
